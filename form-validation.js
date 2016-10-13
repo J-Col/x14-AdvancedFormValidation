90f06ff8c0915b52c8336bc3cce4e9005bca0988
Joshua Colclazier <jcolclazier@mail.csuchico.edu>
2016-10-12 17:21:07 -0700

    Final
    Two new functions for validation checks

--- form-validation.js
+++ form-validation.js
@@ -2,26 +2,52 @@ function validate(){
   //Grab the user's input and store in variables
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
+  var notification = "";
 
   //Show message that there is an error with the username...
-  if (userEntered.length >= 6) {
+  if ((notification = validUser(userEntered)) =="great") {
     document.getElementById("usernameGroup").classList.add("has-success");
   }
   else {
-    document.getElementById("usernameError").innerHTML="Bad username.";
+    document.getElementById("usernameError").innerHTML=notification;
     document.getElementById("usernameError").classList.remove("hidden-message");
     document.getElementById("usernameError").classList.add("shown-message");
     document.getElementById("usernameGroup").classList.add("has-error");
   }
   //Show message that there is an error with the password...
-  if(passEntered != "password") {
+  if((notification = validPass(passEntered, userEntered)) =="great") {
     document.getElementById("passwordGroup").classList.add("has-success");
   }
   else {
-  document.getElementById("passwordError").innerHTML="Bad password.";
+  document.getElementById("passwordError").innerHTML=notification;
   document.getElementById("passwordError").classList.remove("hidden-message");
   document.getElementById("passwordError").classList.add("shown-message");
   //Turn the password items red
   document.getElementById("passwordGroup").classList.add("has-error");
   }
+}
+
+function validUser(username) {
+  if (username.length < 6) {
+    return "Username must be 6 characters or longer";
+  }
+  if (username.includes(" ")) {
+      return "username cannot contain spaces";
+  }
+  else
+    return "great";
+}
+
+function validPass(password, username) {
+  if (password.toLowerCase() == "password") {
+    return "password must be different than password";
+  }
+  if (password == username) {
+    return "Cannot be same as username";
+  }
+  if (password.length < 6 || password.length > 20) {
+    return "password must be 6-20 characters";
+  }
+  else 
+    return "great";
 }
